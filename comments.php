1f8dee2ace9ba9315e56ad3495827367442b3b2c
Moad Qarbal <mouadqarbal@gmail.com>
2020-08-04 17:09:30 +0000

    single page + comments

---
+++ comments.php
@@ -0,0 +1,58 @@
+<?php
+
+if(comments_open())
+{
+?>
+
+	<h3 class="comment-counts">
+		<?php comments_number('0 Commentaire','1 Commentaire','% Commentaires') ?>
+	</h3>
+
+<?php
+	echo '<ul class="list-unstyled comments-list">';
+
+	$comments_args = array(
+		'max_depth' => 3,
+		'type' => 'comment',
+		'avatar_size' => 64
+	);
+
+	wp_list_comments($comments_args);
+
+	echo '</ul>';
+
+	$comment_form = array(
+		'fields' => array(
+			'author' => '<div class="form-group">
+							<label>Votre Nom et Prenom </label>
+							<input id="author" name="author" size="30"
+							 maxlength="245" required="required" class="form-control" type="text">
+						</div>',
+			'email' => '<div class="form-group">
+							<label>Votre Email </label>
+							<input id="email" name="email" size="30"
+							 maxlength="100" aria-describedby="email-notes"
+							 required="required" class="form-control" type="text">
+						</div>',
+			'url' => '<div class="form-group">
+							<label>Votre site web </label>
+							<input id="url" name="url" 
+							 size="30" maxlength="200" class="form-control" type="text">
+						</div>',
+		),
+		'comment_field' => '<div class="form-group">
+								<label>Commentaire </label>
+								<textarea id="comment" name="comment" cols="45" rows="8" 
+								maxlength="65525" required="required" class="form-control"></textarea>
+							</div>',
+        'title_reply' => 'Écrivez votre commentaire',
+        'title_reply_to' => 'Ajouter une réponse à [%s]',
+        'comment_notes_before' => ''
+	);
+
+	comment_form($comment_form);
+
+}else
+{
+	echo 'comments are closed';
+}
--- single.php
+++ single.php
@@ -6,10 +6,16 @@
             <section class="single-page">
             <div class="row">
                 <div class="col text-center m-0 p-0" class="title-header">
-                    <img src="https://via.placeholder.com/900" class="w-100 title-header-img" alt="test">
+	                <?php if(has_post_thumbnail()): ?>
+		                <?php the_post_thumbnail('blog-single-thumb',array('class'=>'w-100 title-header-img')) ?>
+	                <?php else: ?>
+                        <img src="https://via.placeholder.com/900" class="w-100 title-header-img">
+	                <?php endif; ?>
                     <div class="title">
                         <h1 class="text-white"><?php the_title() ?></h1>
-                        <span class="text-white">Creer Par : Moad | En : 15-12-2018</span>
+                        <span class="text-white">Creer Par : <a class="author text-white" href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>">
+                                        <?php the_author(); ?>
+                                    </a> | En : <?php the_date() ?></span>
                     </div>
                     <div class="overlay"></div>
                 </div>
@@ -18,28 +24,24 @@
             <div class="container">
                 <div class="row py-5">
                     <div class="col-md-12 main-post">
-                        <p>
-                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has
-                            survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently
-                            with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                        </p>
-                        <a href="#"> hello world </a>
-                        <h2>Title number one</h2>
-                        <ul>
-                            <li>test one</li>
-                            <li>test two</li>
-                            <li>test three</li>
-                            <li>test four</li>
-                        </ul>
-                        <img src="https://via.placeholder.com/900" alt="test">
+                        <?php the_content() ?>
                     </div>
 
                     <div class="col-md-12 my-4">
                         <div class="tags">
                             <h3>Les Tags</h3>
-                            <a href="#" class="badge badge-pill mx-1 px-3 text-white py-2 bg-dark">hello</a>
-                            <a href="#" class="badge badge-pill mx-1 px-3 text-white py-2 bg-dark">hello</a>
-                            <a href="#" class="badge badge-pill mx-1 px-3 text-white py-2 bg-dark">hello</a>
+                            <div class="tags mt-4">
+		                        <?php
+		                        if ( is_singular() ) :
+			                        $ordered_tag_list = wp_get_post_terms(get_the_ID(), 'post_tag', array('orderby' => 'term_id', 'fields' => 'all'));
+			                        if($ordered_tag_list == []){ echo 'sans tags'; }
+			                        foreach($ordered_tag_list as $tag) {
+				                        echo '<a class="badge badge-pill mx-1
+                                                    px-3 text-white py-2 bg-main bg-dark" href="' . get_term_link( $tag ) . '">' . esc_html( $tag->name ) . '</a>';
+			                        }
+		                        endif;
+		                        ?>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -47,6 +49,77 @@
         </section>
         <?php endwhile; ?>
     <?php endif; ?>
+
+    <section class="related-posts">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h2 class="mb-4">Articles Similaires</h2>
+                </div>
+
+	            <?php
+
+	            $related = new WP_Query(
+		            array(
+			            'category__in' => wp_get_post_categories($post->ID),
+			            'numberposts' => 4,
+			            'post__not_in' => array($post->ID)
+		            )
+	            );
+	            ?>
+
+	            <?php if ($related->have_posts()): ?>
+		            <?php while ($related->have_posts()): $related->the_post() ?>
+                        <div class="col-md-6 col-sm-12">
+                            <div class="card mb-3">
+                                <div class="row no-gutters">
+                                    <div class="col-md-4">
+                                        <a href="<?php the_permalink() ?>">
+								            <?php if(has_post_thumbnail()): ?>
+									            <?php the_post_thumbnail('blog-thumb-related',array('class'=>'card-img-related w-100')) ?>
+								            <?php else: ?>
+                                                <img src="https://via.placeholder.com/900" class="card-img-related w-100">
+								            <?php endif; ?>
+                                        </a>
+                                    </div>
+                                    <div class="col-md-8">
+                                        <div class="card-body">
+                                            <p class="card-title">
+                                                <a href="<?php the_permalink() ?>"
+                                                   class="title-3 text-dark text-capitalize">
+										            <?php the_title() ?>
+                                                </a>
+                                            </p>
+                                            <p class="card-text text-muted">
+									            <small>
+										            <?php echo excerpt(17) ?>
+                                                </small>
+                                            </p>
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+		            <?php endwhile; ?>
+	            <?php else: ?>
+		            <?php echo wpautop('No Posts Yet!'); ?>
+	            <?php endif; ?>
+            </div>
+        </div>
+    </section>
+
+    <section class="comments my-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="comments-box">
+				        <?php  comments_template();  ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 
 
 <?php get_footer() ?>
